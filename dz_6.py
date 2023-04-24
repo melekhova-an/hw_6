02a9e55631adda03fdddb6e0c0c4381791d2717b
Anastasiya Melekhova <amelekhova-aan@yandex.ru>
2023-04-24 14:16:11 +0300

    fix hw

--- dz_6.py
+++ dz_6.py
@@ -88,15 +88,15 @@ def function_name(func, *args):
 
 def open_browser(browser_name):
     actual_result = function_name(open_browser, browser_name)
-
+    print(actual_result)
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
     actual_result = function_name(go_to_companyname_homepage, page_url)
-
+    print(actual_result)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = function_name(find_registration_button_on_login_page, page_url, button_text)
-
+    print(actual_result)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
